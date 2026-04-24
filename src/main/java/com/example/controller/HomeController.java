0404dcf5de07ad2f7ec865b0836fc094019825d6
Rutuja Lokhande <ruttuja8308@gmail.com>
2026-04-24 17:08:45 +0530

    commit by Rutuja

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -9,6 +9,6 @@ public class HomeController
 	@GetMapping("/home")
 	public String home()
 	{
-		return "Deployment is done ! Updated once";
+		return "Deployment is done ! Updated once by Rutuja";
 	}
 }
