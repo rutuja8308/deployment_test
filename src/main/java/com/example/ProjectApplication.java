7cd394e8ca55d986ae1edbdb6b65d5b15d5eac1a
Rutuja Lokhande <ruttuja8308@gmail.com>
2026-04-24 15:47:05 +0530

    first commit

---
+++ src/main/java/com/example/ProjectApplication.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ProjectApplication extends SpringBootServletInitializer
+{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ProjectApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
